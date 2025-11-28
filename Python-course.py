a735138d5036b79870e503a26b5ed8d53316d5c5
GeonwooLee <96867850+Jamesonthehill@users.noreply.github.com>
2025-11-28 04:23:11 +0000

    loop sentence

--- Python-course.py
+++ Python-course.py
@@ -3,5 +3,25 @@ Alpha = 'Mario'
 ss = '11'
 
 
-print(Alpha + str(Integer))
-print(10 + int(ss))
+# print(Alpha + str(Integer))
+# print(10 + int(ss))
+
+# age = 18
+# isHappy = False
+
+# if age > 29:
+#     print('you are old!')
+# elif age >= 18:
+#     print('you are getting old!')
+# else:
+#     print('you are still young!')
+    
+# if isHappy:
+#     print('Happy Thankgiving')
+# else: 
+#     print('Proud of yourself')
+
+i = 124
+# even if we assign the i value, if we use again, it would be overwritten.
+for i in range(3):
+        print('Hello', i+3)
