01c5db95f27d74afaebfb2a5780660923578f316
GeonwooLee <96867850+Jamesonthehill@users.noreply.github.com>
2025-12-05 06:01:51 +0000

    leetscode Merge Sorted Array

--- Python-course.py
+++ Python-course.py
@@ -56,3 +56,24 @@ while True:
 # except:
 #     print('that is not a valid number!')
 
+
+
+num1 = [1,2,3]
+num2 = [4,5,6]
+merge_array = num1 + num2
+print(merge_array)
+
+
+## leetscode pattern 
+i = m - 1
+j = n - 1
+k = m + n - 1
+
+while j >= 0:
+    if i >= 0 and nums1[i] >= nums2[j]:
+        nums1[k] = nums1[i]
+        i -= 1
+    else:
+        nums1[k] = nums2[j]
+        j -= 1
+    k -= 1
