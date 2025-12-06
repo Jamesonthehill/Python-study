9ceb6770aa4bd261bbe352fd3be17f9861a86477
GeonwooLee <96867850+Jamesonthehill@users.noreply.github.com>
2025-12-06 01:32:53 +0000

    leetscode Remove Element

--- Python-course.py
+++ Python-course.py
@@ -82,3 +82,15 @@ print(merge_array)
 alpha = [1, 2]
 alphaka = len(alpha)
 print(alphaka)
+
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = len(nums) - 1
+
+#         while i >= 0:
+#             if nums[i] == val:
+#                 del nums[i]
+#                 i -= 1
+#             else:
+#                 nums[i] == nums[i]
+#                 i -= 1
