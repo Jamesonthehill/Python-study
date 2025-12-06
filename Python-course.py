4a3f1a14f4bbd582c9aed9c32fca4ef874702494
GeonwooLee <96867850+Jamesonthehill@users.noreply.github.com>
2025-12-06 01:17:48 +0000

    len(alphaka)

--- Python-course.py
+++ Python-course.py
@@ -65,15 +65,20 @@ print(merge_array)
 
 
 ## leetscode pattern 
-i = m - 1
-j = n - 1
-k = m + n - 1
+# i = m - 1
+# j = n - 1
+# k = m + n - 1
 
-while j >= 0:
-    if i >= 0 and nums1[i] >= nums2[j]:
-        nums1[k] = nums1[i]
-        i -= 1
-    else:
-        nums1[k] = nums2[j]
-        j -= 1
-    k -= 1
+# while j >= 0:
+#     if i >= 0 and nums1[i] >= nums2[j]:
+#         nums1[k] = nums1[i]
+#         i -= 1
+#     else:
+#         nums1[k] = nums2[j]
+#         j -= 1
+#     k -= 1
+
+## Leetscode Remove Element
+alpha = [1, 2]
+alphaka = len(alpha)
+print(alphaka)
