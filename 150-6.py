7d62c30851157470d6ad64d51856600a8f51dc09
geonwoolee <dlrjsdn53@naver.com>
2025-12-13 17:21:48 -0500

    150-6 Rotate Array

--- 150-6.py
+++ 150-6.py
@@ -2,7 +2,9 @@
 # 150-6 Rotate Array
 nums = [1,2,3,4,5,6,7]
 i = 0 
-for x in nums:
-    nums.insert((i + 3) % len(nums), x)
-    i += 1
-    del nums[i]
+print(nums[::2])    # every 2nd element
+print(nums[::-1])   # reversed copy of the list
+
+nums[:] = nums[-3:] + nums[:-3]
+print(nums[:])
+
