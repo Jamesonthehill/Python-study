f70011cb086388133043111dea18e86a944d4f13
geonwoolee <dlrjsdn53@naver.com>
2025-12-22 15:56:16 -0500

    150-6 Rotate Array

--- 150-6.py
+++ 150-6.py
@@ -2,6 +2,15 @@
 # 150-6 Rotate Array
 nums = [1,2,3,4,5,6,7]
 i = 0 
+print("-----")
+print(nums[4:])
+print(nums[:-3])
+print(nums[3:])
+print(nums[-3:])
+print(nums[:2])
+print(nums[:-2])
+print("------")
+
 print(nums[::2])    # every 2nd element
 print(nums[::-1])   # reversed copy of the list
 
