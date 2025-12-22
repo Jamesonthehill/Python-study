3adf94b23d1416e61e67fe804cc8891efa620456
geonwoolee <dlrjsdn53@naver.com>
2025-12-21 20:25:11 -0500

    .

--- 150-7.py
+++ 150-7.py
@@ -1,21 +1,34 @@
-prices = [7,1,5,3,6,4]
-max = 0
-min = 0
+prices = [2,4,1]
+
+# i = 0
+# max = 0
+# min = prices[i]
+# for i in len(prices):
+#     if prices[i]
 x = 0
 i = 0
+total = 0
+max = prices[x]
+min = prices[i]
 while i < len(prices):
-    if i < 1 or prices[i] < min:
+    if prices[i] < min:
         min = prices[i]
+        total = max - min
         x = i
         if i == len(prices)-1:
                 break
     i += 1
 
-while x < len(prices):
     if prices[x] > max: 
         max = prices[x]
         if x == len(prices)-1:
             break
     x += 1
+print(total)
 
-print(max-min)
+# while x < len(prices):
+#     if prices[x] > max: 
+#         max = prices[x]
+#         if x == len(prices)-1:
+#             break
+#     x += 1
