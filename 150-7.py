87687bc958a45400cc01e00eccc6b6efeb02b6c7
geonwoolee <dlrjsdn53@naver.com>
2025-12-21 23:55:41 -0500

    Best time to buy the sell stock

--- 150-7.py
+++ 150-7.py
@@ -1,34 +1,19 @@
 prices = [1]
-
-# i = 0
-# max = 0
-# min = prices[i]
-# for i in len(prices):
-#     if prices[i]
-x = 0
 i = 0
-total = 0
+min = prices[0]
 max = 0
-min = prices[i]
 while i < len(prices):
-    if i < 1 or prices[i] < min:
-        min = prices[i]
-    if len(prices) == 1:
-        total = 0
-        break
+    price = prices[i]
+   
+    if price < min:
+        min = price
+
+    else:
+        profit = price - min
+        if profit > max: 
+            max = profit
     i += 1
-    x = i
-    if x < 1 or prices[x] > max: 
-        max = prices[x]
-        total = max - min
-        if total <= 0:
-            total = 0
-    if x == len(prices)-1:
-        break
-
-    x += 1
-print(total)
-
+print(max)
 # while x < len(prices):
 #     if prices[x] > max: 
 #         max = prices[x]
