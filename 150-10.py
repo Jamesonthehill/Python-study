72496179ada6a18e116c214196b5f953ab83658a
geonwoolee <dlrjsdn53@naver.com>
2026-01-15 00:31:18 -0500

    45. Jump Game II

--- 150-10.py
+++ 150-10.py
@@ -1,1 +1,30 @@
-         
+from typing import List
+i = 0
+nums = [1,2,1,1,1]
+n = len(nums) - 1
+jump_count = 0
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        i = 0
+        n = len(nums) - 1
+        jump_count = 0
+        # while i < n-i:
+        for i in range(0, len(nums)-1):
+            
+            if len(nums) == 1 or nums[i] == 0:
+                i = n
+                break
+            
+            if nums[i] + i >= n:
+                jump_count += 1
+                i = len(nums)
+                break
+        
+            else:
+                jump_count += 1
+        
+        return jump_count
+    
+print(jump_count)
