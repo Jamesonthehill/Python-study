4c7ff7ab2cf6c3ec0af30fca7b1350fb60d8f258
geonwoolee <dlrjsdn53@naver.com>
2026-01-17 20:33:16 -0500

    Jump Game II answer

--- 150-10.py
+++ 150-10.py
@@ -1,22 +1,27 @@
 from typing import List
+nums = [1,2,3]
 i = 0
-nums = [1,2,1,1,1]
 n = len(nums) - 1
 jump_count = 0
-farthest = nums[0]
-while i < len(nums):
+farthest = nums[i]
+current_end = 0
+while i < len(nums)-1: # iteration until meeting the requirement.
     
-    farthest = max(farthest, nums[i])
-
-    if len(nums) == 1 or nums[i] == 0:
-        i = n
-        break
-    
-    if farthest >= n:
+    farthest = max(farthest, i + nums[i])
+    if len(nums) == 1: # exception 
         i = len(nums)
         break
 
-    jump_count += farthest
-    i += 1 
+    if  i == current_end:
+        jump_count += 1
+        current_end = farthest
 
-print(i)
+    if current_end >= n:
+        i = len(nums)
+        break
+
+    
+    i += 1
+
+
+print(jump_count)
