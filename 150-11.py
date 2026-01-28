2460839064c34dac21b036744e202b6f265f05d8
geonwoolee <dlrjsdn53@naver.com>
2026-01-27 20:16:18 -0500

    error

--- 150-11.py
+++ 150-11.py
@@ -1,1 +1,14 @@
 # Hindex     
+import math
+citations = [3,0,6,1,5]
+i = 0 
+total = 0
+number = 0
+for i in range(0, len(citations)):
+    total += citations[i]
+
+    if i == len(citations)-1:
+        number = total / len(citations)
+        number = math.trunc(number)
+
+print(number)
