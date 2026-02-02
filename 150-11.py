2a4df5e3d2b86b46a9856f799527fd489dd4054b
geonwoolee <dlrjsdn53@naver.com>
2026-02-02 15:05:20 -0500

    test

--- 150-11.py
+++ 150-11.py
@@ -1,16 +1,17 @@
 # Hindex     
-import math
 citations = [3,0,6,1,5]
-total = 0
-tempo = 0
+total = []
+
+citations.sort(reverse=True)
+Ascend = citations
+print(Ascend)
+print(citations)
+count = 0
+i = 0
 final = 0
-for i in range(len(citations), 0):
-    
-    # H-index cannot be bigger than len[citations]
+while i < len(citations):
+    if Ascend[i] >= i+1:
+        final = Ascend[i]
+    i += 1
 
-
-    if i == len(citations)-1:
-        final = total / len(citations)
-        final = math.trunc(final)
-
-print(final)   
+print(final)
